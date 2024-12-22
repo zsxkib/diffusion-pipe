zsxkib/diffusion-pipe

commit 5c1b48649654ae71e064b00ef6310629973b0591

Fix open file leak causing crash
See https://github.com/pytorch/pytorch/issues/91252
When a DataLoader is created using this combination of arguments, it leaks
an open file until program exit. This commit moves things around to avoid
recreating the dataloader on each iteration.

utils/dataset.py
@@ -640,19 +640,23 @@ class PipelineDataLoader:
         self.skip_first_n_batches = None
         self.iter_called = False
         self.eval_quantile = None
-        self.reset()
+        self.epoch = 1
+        self.num_batches_pulled = 0
+        self.next_micro_batch = None
+        # Be careful to only create the DataLoader some bounded number of times: https://github.com/pytorch/pytorch/issues/91252
+        self._create_dataloader()
 
     def reset(self):
         self.epoch = 1
         self.num_batches_pulled = 0
         self.next_micro_batch = None
+        self.data = self._pull_batches_from_dataloader()
 
     def set_eval_quantile(self, quantile):
         self.eval_quantile = quantile
 
     def __iter__(self):
         self.iter_called = True
-        self._create_dataloader()
         return self
 
     def __len__(self):
@@ -666,7 +670,7 @@ class PipelineDataLoader:
             self.next_micro_batch = next(self.data)
         except StopIteration:
             assert self.skip_first_n_batches is None
-            self._create_dataloader()
+            self.data = self._pull_batches_from_dataloader()
             self.num_batches_pulled = 0
             self.next_micro_batch = next(self.data)
             self.epoch += 1
@@ -720,6 +724,7 @@ class PipelineDataLoader:
         # pulled than the actual number of batches iterated by the caller.
         self.num_batches_pulled = state_dict['num_batches_pulled'] - 1
         self.skip_first_n_batches = self.num_batches_pulled
+        self._create_dataloader()
 
 
 class SkipFirstNSampler(torch.utils.data.Sampler):

train.py
@@ -177,11 +177,6 @@ if __name__ == '__main__':
     # needed for broadcasting Queue in dataset.py
     mp.current_process().authkey = b'afsaskgfdjh4'
 
-    # try to fix "OSError: [Errno 24] Too many open files" that can happen when running eval
-    # https://github.com/pytorch/pytorch/issues/11201
-    import torch.multiprocessing
-    torch.multiprocessing.set_sharing_strategy('file_system')
-
     with open(args.config) as f:
         # Inline TOML tables are not pickleable, which messes up the multiprocessing dataset stuff. This is a workaround.
         config = json.loads(json.dumps(toml.load(f)))

commit 29ca439fecf68b7dcb00312e1e474f22044475a9

Fix bug causing first part of dataset to be consistently skipped with resuming from checkpoint
Bug was introduced in 5c1b48649654ae71e064b00ef6310629973b0591. We no longer recreate
the dataloader each iteration. But when resuming from checkpoint, we make a dataloader
that skips the first n batches (because we checkpointed part way through an epoch). So
now, when resuming, we have to make sure to recreate the dataloader just once after the
first iteration so it only skips some examples once.

utils/dataset.py
@@ -664,14 +664,15 @@ class PipelineDataLoader:
         self.dataset = dataset
         self.gradient_accumulation_steps = gradient_accumulation_steps
         self.num_dataloader_workers = num_dataloader_workers
-        self.skip_first_n_batches = None
         self.iter_called = False
         self.eval_quantile = None
         self.epoch = 1
         self.num_batches_pulled = 0
         self.next_micro_batch = None
+        self.recreate_dataloader = False
         # Be careful to only create the DataLoader some bounded number of times: https://github.com/pytorch/pytorch/issues/91252
         self._create_dataloader()
+        self.data = self._pull_batches_from_dataloader()
 
     def reset(self):
         self.epoch = 1
@@ -696,17 +697,18 @@ class PipelineDataLoader:
         try:
             self.next_micro_batch = next(self.data)
         except StopIteration:
-            assert self.skip_first_n_batches is None
+            if self.recreate_dataloader:
+                self._create_dataloader()
+                self.recreate_dataloader = False
             self.data = self._pull_batches_from_dataloader()
             self.num_batches_pulled = 0
             self.next_micro_batch = next(self.data)
             self.epoch += 1
         return ret
 
-    def _create_dataloader(self):
-        if self.skip_first_n_batches is not None:
-            sampler = SkipFirstNSampler(self.skip_first_n_batches, len(self.dataset))
-            self.skip_first_n_batches = None
+    def _create_dataloader(self, skip_first_n_batches=None):
+        if skip_first_n_batches is not None:
+            sampler = SkipFirstNSampler(skip_first_n_batches, len(self.dataset))
         else:
             sampler = None
         self.dataloader = torch.utils.data.DataLoader(
@@ -717,7 +719,6 @@ class PipelineDataLoader:
             num_workers=self.num_dataloader_workers,
             persistent_workers=(self.num_dataloader_workers > 0),
         )
-        self.data = self._pull_batches_from_dataloader()
 
     def _pull_batches_from_dataloader(self):
         for batch in self.dataloader:
@@ -750,8 +751,11 @@ class PipelineDataLoader:
         # -1 because by preloading the next micro_batch, it's always going to have one more batch
         # pulled than the actual number of batches iterated by the caller.
         self.num_batches_pulled = state_dict['num_batches_pulled'] - 1
-        self.skip_first_n_batches = self.num_batches_pulled
-        self._create_dataloader()
+        self._create_dataloader(skip_first_n_batches=self.num_batches_pulled)
+        self.data = self._pull_batches_from_dataloader()
+        # Recreate the dataloader after the first pass so that it won't skip
+        # batches again (we only want it to skip batches the first time).
+        self.recreate_dataloader = True
 
 
 class SkipFirstNSampler(torch.utils.data.Sampler):