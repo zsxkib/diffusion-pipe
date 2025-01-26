Fix SDXL pipeline parallelism and switch to non-reentrant gradient checkpointing
After the SDXL pipeline parallelism fix, I was getting errors like
"element 1 of tensors does not require grad and does not have a grad_fn".
Even trying to force a bunch of tensors to require grad in places didn't
fix it. But switching to non-reentrant activation checkpoint worked.
Non-reentrant is the recommended option in Pytorch, but not the default
in deepspeed. I tested all supported models and they are able to train.
Hopefully it's safe to make this change globally.

models/ltx_video.py
@@ -143,9 +143,6 @@ class InitialLayer(nn.Module):
 
     @torch.autocast('cuda', dtype=AUTOCAST_DTYPE)
     def forward(self, inputs):
-        for item in inputs:
-            if torch.is_floating_point(item):
-                item.requires_grad_(True)
         hidden_states, encoder_hidden_states, encoder_attention_mask, timestep, num_frames, height, width, rope_interpolation_scale_time, rope_interpolation_scale_space, target = inputs
 
         rope_interpolation_scale = (
@@ -178,7 +175,11 @@ class InitialLayer(nn.Module):
         encoder_hidden_states = self.caption_projection(encoder_hidden_states)
         encoder_hidden_states = encoder_hidden_states.view(batch_size, -1, hidden_states.size(-1))
 
-        return make_contiguous(hidden_states, encoder_hidden_states, temb, embedded_timestep, freqs_cos, freqs_sin, encoder_attention_mask, target)
+        outputs = make_contiguous(hidden_states, encoder_hidden_states, temb, embedded_timestep, freqs_cos, freqs_sin, encoder_attention_mask, target)
+        for tensor in outputs:
+            if torch.is_floating_point(tensor):
+                tensor.requires_grad_(True)
+        return outputs
 
 
 class TransformerLayer(nn.Module):

models/sdxl.py
@@ -302,7 +302,7 @@ class InitialLayer(nn.Module):
                 # Forward upsample size to force interpolation output size.
                 forward_upsample_size = True
                 break
-        forward_upsample_size = torch.tensor(forward_upsample_size)
+        forward_upsample_size = torch.tensor(forward_upsample_size).to(sample.device)
 
         prompt_embeds_list = []
         for text_input_ids, text_encoder in [(input_ids, self.text_encoder), (input_ids_2, self.text_encoder_2)]: